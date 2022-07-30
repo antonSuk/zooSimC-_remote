b45f9b385051da7d6d5aca21055e224a433cc392
AntonS <anton100100@live.com>
2022-07-31 00:06:37 +0100

    Initial Commit

---
+++ zooSim/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  zooSim
+//
+//  Created by Anton Sukhorukov on 31/07/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
