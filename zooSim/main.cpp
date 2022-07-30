6d1a037fef2a22b6e0706f61b168961fe47b361d
AntonS <anton100100@live.com>
2022-07-31 00:14:21 +0100

    New welcome message added "Hello, Welcome to the zoo simulator".

--- zooSim/main.cpp
+++ zooSim/main.cpp
@@ -8,7 +8,11 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    std::cout << "Hello, welcome to the Zoo simulator!\n";
+    
+    
+    
+    
     return 0;
 }
